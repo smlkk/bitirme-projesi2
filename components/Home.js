22899e0a9bfe9b05fd366abd9e9976bd43808920
smlkk <151566662+smlkk@users.noreply.github.com>
2024-02-11 01:58:55 +0300

    Home.js

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Hoşgeldiniz!</h2>
+      <p>Belediyemizin yaklaşan etkinliklerini keşfedin.</p>
+    </div>
+  );
+};
+
+export default Home;
